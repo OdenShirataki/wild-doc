fed9438a6f6910d7e6c03517297530cc8eac25b6
Oden <oden.srtk@gmail.com>
2022-12-31 16:44:36 +0900

    use Path and PathBuf

--- src/include.rs
+++ src/include.rs
@@ -1,30 +1,40 @@
-use std::{collections::HashMap, io::Read};
+use std::{
+    collections::HashMap,
+    io::Read,
+    path::{Path, PathBuf},
+};
 
 pub trait IncludeAdaptor {
-    fn include(&mut self, path: &str) -> &str;
+    fn include<P: AsRef<Path>>(&mut self, path: P) -> &str;
 }
 pub struct IncludeLocal {
-    dir: String,
-    cache: HashMap<String, String>,
+    dir: PathBuf,
+    cache: HashMap<PathBuf, String>,
 }
 impl IncludeLocal {
-    pub fn new(dir: impl Into<String>) -> Self {
+    pub fn new<P: AsRef<Path>>(dir: P) -> Self {
         Self {
-            dir: dir.into(),
+            dir: dir.as_ref().to_path_buf(),
             cache: HashMap::default(),
         }
     }
 }
 impl IncludeAdaptor for IncludeLocal {
-    fn include(&mut self, path: &str) -> &str {
-        self.cache.entry(path.to_owned()).or_insert_with_key(|path| {
-            if let Ok(mut f) = std::fs::File::open(&(self.dir.clone() + path)) {
-                let mut contents = String::new();
-                let _ = f.read_to_string(&mut contents);
-                contents
-            } else {
-                "".to_string()
-            }
-        })
+    fn include<P: AsRef<Path>>(&mut self, path: P) -> &str {
+        let path = path.as_ref().to_path_buf();
+        self.cache
+            .entry(path.to_owned())
+            .or_insert_with_key(|path| {
+                let mut file_path = self.dir.clone();
+                file_path.push(&path);
+
+                if let Ok(mut f) = std::fs::File::open(file_path) {
+                    let mut contents = String::new();
+                    let _ = f.read_to_string(&mut contents);
+                    contents
+                } else {
+                    "".to_string()
+                }
+            })
     }
 }
