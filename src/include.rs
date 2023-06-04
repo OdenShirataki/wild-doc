3db2955d56b784bc9d41eeae3cfcef3ed1ea16f4
OdenShirataki <oden.srtk@gmail.com>
2023-06-04 09:51:38 +0900

    feat!: change Rc to Arc
    in fn include of IncludeAdaptor

--- src/include.rs
+++ src/include.rs
@@ -2,15 +2,15 @@ use std::{
     collections::HashMap,
     io::Read,
     path::{Path, PathBuf},
-    rc::Rc,
+    sync::Arc,
 };
 
 pub trait IncludeAdaptor {
-    fn include<P: AsRef<Path>>(&mut self, path: P) -> Option<Rc<Vec<u8>>>;
+    fn include<P: AsRef<Path>>(&mut self, path: P) -> Option<Arc<Vec<u8>>>;
 }
 pub struct IncludeLocal {
     dir: PathBuf,
-    cache: HashMap<PathBuf, Rc<Vec<u8>>>,
+    cache: HashMap<PathBuf, Arc<Vec<u8>>>,
 }
 impl IncludeLocal {
     pub fn new<P: AsRef<Path>>(dir: P) -> Self {
@@ -21,7 +21,7 @@ impl IncludeLocal {
     }
 }
 impl IncludeAdaptor for IncludeLocal {
-    fn include<P: AsRef<Path>>(&mut self, path: P) -> Option<Rc<Vec<u8>>> {
+    fn include<P: AsRef<Path>>(&mut self, path: P) -> Option<Arc<Vec<u8>>> {
         let path = path.as_ref();
         if !self.cache.contains_key(path) {
             let mut file_path = self.dir.clone();
@@ -29,7 +29,7 @@ impl IncludeAdaptor for IncludeLocal {
             if let Ok(mut f) = std::fs::File::open(file_path) {
                 let mut contents = Vec::new();
                 if let Ok(_) = f.read_to_end(&mut contents) {
-                    self.cache.insert(path.to_owned(), Rc::new(contents));
+                    self.cache.insert(path.to_owned(), Arc::new(contents));
                 }
             }
         }
