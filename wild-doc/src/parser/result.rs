7584613d060c5323c793940088606317ce7e288e
OdenShirataki <oden.srtk@gmail.com>
2023-09-07 23:25:59 +0900

    refactor: optimize

--- wild-doc/src/parser/result.rs
+++ wild-doc/src/parser/result.rs
@@ -48,35 +48,39 @@ impl Parser {
                             break;
                         }
                     }
-                    let json_rows = if let Some(session) = session_maybe_has_collection {
-                        session.search(&search)
-                            .result(&self.database.read().unwrap(), &orders)?
-                            .iter()
-                            .map(|row| {
-                                let mut json_row = Map::new();
-                                json_row.insert("row".to_owned(), json!(row));
-                                Value::Object(json_row)
-                            })
-                            .collect::<Vec<Value>>()
-                    } else {
-                        search
-                            .write()
-                            .unwrap()
-                            .result(&self.database.read().unwrap())
-                            .read()
-                            .unwrap()
-                            .as_ref()
-                            .map_or(vec![], |v| v.sort(&self.database.read().unwrap(), &orders))
-                            .iter()
-                            .map(|row| {
-                                Value::Object({
+                    let json_rows: Vec<_> = session_maybe_has_collection.map_or_else(
+                        || {
+                            search
+                                .write()
+                                .unwrap()
+                                .result(&self.database.read().unwrap())
+                                .read()
+                                .unwrap()
+                                .as_ref()
+                                .map_or(vec![], |v| v.sort(&self.database.read().unwrap(), &orders))
+                                .iter()
+                                .map(|row| {
+                                    Value::Object({
+                                        let mut json_row = Map::new();
+                                        json_row.insert("row".to_owned(), json!(row));
+                                        json_row
+                                    })
+                                })
+                                .collect()
+                        },
+                        |session| {
+                            session
+                                .search(&search)
+                                .result(&self.database.read().unwrap(), &orders)
+                                .iter()
+                                .map(|row| {
                                     let mut json_row = Map::new();
                                     json_row.insert("row".to_owned(), json!(row));
-                                    json_row
+                                    Value::Object(json_row)
                                 })
-                            })
-                            .collect::<Vec<Value>>()
-                    };
+                                .collect()
+                        },
+                    );
                     let len = json_rows.len();
                     json_inner.insert("rows".to_owned(), Value::Array(json_rows));
                     json_inner.insert("len".to_owned(), json!(len));
@@ -101,7 +105,7 @@ fn make_order<'a>(search: &Arc<RwLock<Search>>, sort: &str) -> Vec<Order> {
             let is_desc = o.ends_with(" DESC");
             let o_split: Vec<&str> = o.split(" ").collect();
             let field = o_split[0];
-            let order_key = if field.starts_with("field.") {
+            if let Some(order_key) = if field.starts_with("field.") {
                 field
                     .strip_prefix("field.")
                     .map(|v| OrderKey::Field(v.to_owned()))
@@ -123,8 +127,7 @@ fn make_order<'a>(search: &Arc<RwLock<Search>>, sort: &str) -> Vec<Order> {
                     "last_update" => Some(OrderKey::LastUpdated),
                     _ => None,
                 }
-            };
-            if let Some(order_key) = order_key {
+            } {
                 orders.push(if is_desc {
                     Order::Desc(order_key)
                 } else {
