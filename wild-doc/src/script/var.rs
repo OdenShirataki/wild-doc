db3530c84d024b06579c8815368047c7f0832785
OdenShirataki <oden.srtk@gmail.com>
2023-10-16 22:29:51 +0900

    fix: global

--- wild-doc/src/script/var.rs
+++ wild-doc/src/script/var.rs
@@ -1,17 +1,16 @@
 use std::sync::Arc;
 
 use anyhow::Result;
-use parking_lot::Mutex;
 
-use wild_doc_script::{async_trait, VarsStack, WildDocScript, WildDocValue};
+use wild_doc_script::{async_trait, WildDocScript, WildDocState, WildDocValue};
 
 pub struct Var {
-    stack: Arc<Mutex<VarsStack>>,
+    state: Arc<WildDocState>,
 }
 
 impl Var {
     fn search_stack(&self, key: &[u8]) -> Option<Arc<WildDocValue>> {
-        for stack in self.stack.lock().iter().rev() {
+        for stack in self.state.stack().lock().iter().rev() {
             if let Some(v) = stack.get(key) {
                 return Some(Arc::clone(v));
             }
@@ -22,12 +21,12 @@ impl Var {
 
 #[async_trait(?Send)]
 impl WildDocScript for Var {
-    fn new(state: Arc<wild_doc_script::WildDocState>) -> Result<Self>
+    fn new(state: Arc<WildDocState>) -> Result<Self>
     where
         Self: Sized,
     {
         Ok(Self {
-            stack: Arc::clone(state.stack()),
+            state: Arc::clone(&state),
         })
     }
 
@@ -40,35 +39,75 @@ impl WildDocScript for Var {
 
         let mut splited = code.split(|c| *c == b'.');
         if let Some(root) = splited.next() {
-            if let Some(ref root) = self.search_stack(root) {
-                let mut next_value = root.as_ref();
-                while {
-                    splited.next().map_or_else(
-                        || {
-                            value = next_value.clone();
-                            false
-                        },
-                        |next| match next_value {
-                            WildDocValue::Object(map) => map
-                                .get(unsafe { std::str::from_utf8_unchecked(next) })
-                                .map_or(false, |v| {
-                                    next_value = v;
-                                    true
-                                }),
-                            WildDocValue::Array(map) => {
-                                unsafe { std::str::from_utf8_unchecked(next) }
-                                    .parse::<usize>()
-                                    .map_or(false, |v| {
-                                        map.get(v).map_or(false, |v| {
+            if root == b"global" {
+                if let Some(global_root) = splited.next() {
+                    if let Some(next_value) = self
+                        .state
+                        .global()
+                        .lock()
+                        .get(unsafe { std::str::from_utf8_unchecked(global_root) })
+                    {
+                        let mut next_value = next_value;
+                        while {
+                            splited.next().map_or_else(
+                                || {
+                                    value = next_value.clone();
+                                    false
+                                },
+                                |next| match next_value {
+                                    WildDocValue::Object(map) => map
+                                        .get(unsafe { std::str::from_utf8_unchecked(next) })
+                                        .map_or(false, |v| {
                                             next_value = v;
                                             true
+                                        }),
+                                    WildDocValue::Array(map) => {
+                                        unsafe { std::str::from_utf8_unchecked(next) }
+                                            .parse::<usize>()
+                                            .map_or(false, |v| {
+                                                map.get(v).map_or(false, |v| {
+                                                    next_value = v;
+                                                    true
+                                                })
+                                            })
+                                    }
+                                    _ => false,
+                                },
+                            )
+                        } {}
+                    }
+                }
+            } else {
+                if let Some(ref root) = self.search_stack(root) {
+                    let mut next_value = root.as_ref();
+                    while {
+                        splited.next().map_or_else(
+                            || {
+                                value = next_value.clone();
+                                false
+                            },
+                            |next| match next_value {
+                                WildDocValue::Object(map) => map
+                                    .get(unsafe { std::str::from_utf8_unchecked(next) })
+                                    .map_or(false, |v| {
+                                        next_value = v;
+                                        true
+                                    }),
+                                WildDocValue::Array(map) => {
+                                    unsafe { std::str::from_utf8_unchecked(next) }
+                                        .parse::<usize>()
+                                        .map_or(false, |v| {
+                                            map.get(v).map_or(false, |v| {
+                                                next_value = v;
+                                                true
+                                            })
                                         })
-                                    })
-                            }
-                            _ => false,
-                        },
-                    )
-                } {}
+                                }
+                                _ => false,
+                            },
+                        )
+                    } {}
+                }
             }
         }
 
