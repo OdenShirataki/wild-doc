b565a74b9b98c278c48d6d5c59b7f390e43db17c
OdenShirataki <oden.srtk@gmail.com>
2023-10-18 23:43:49 +0900

    fix!: uint8array to vec[u8]

--- wild-doc-script-deno/src/lib.rs
+++ wild-doc-script-deno/src/lib.rs
@@ -1,13 +1,14 @@
 pub mod module_loader;
 
-use std::{ffi::c_void, path::Path, sync::Arc};
+use std::{ffi::c_void, ops::Deref, path::Path, sync::Arc};
 
 use deno_runtime::{
     deno_core::{self, anyhow::Result, serde_v8, ModuleSpecifier},
-    deno_napi::v8::{self, NewStringType, PropertyAttribute},
+    deno_napi::v8::{self, HandleScope, NewStringType, PropertyAttribute},
     permissions::PermissionsContainer,
     worker::{MainWorker, WorkerOptions},
 };
+use indexmap::IndexMap;
 use parking_lot::Mutex;
 
 use wild_doc_script::{
@@ -18,6 +19,75 @@ use module_loader::WdModuleLoader;
 
 pub struct Deno {
     worker: Mutex<MainWorker>,
+}
+
+fn wdmap2v8obj<'s>(
+    wdv: &IndexMap<String, WildDocValue>,
+    scope: &'s mut HandleScope,
+) -> v8::Local<'s, v8::Object> {
+    let root = v8::Object::new(scope);
+    let mut obj_stack: Vec<(v8::Local<v8::Object>, &IndexMap<String, WildDocValue>)> =
+        vec![(root, wdv)];
+
+    loop {
+        if let Some((current_obj, current_values)) = obj_stack.pop() {
+            for (key, value) in current_values {
+                if let Some(v8_key) = v8::String::new(scope, key) {
+                    match value {
+                        WildDocValue::Binary(v) => {
+                            let ab = v8::ArrayBuffer::with_backing_store(
+                                scope,
+                                &v8::ArrayBuffer::new_backing_store_from_vec(v.to_owned())
+                                    .make_shared(),
+                            );
+                            if let Some(v) = v8::Uint8Array::new(scope, ab, 0, ab.byte_length()) {
+                                current_obj.set(scope, v8_key.into(), v.into());
+                            }
+                        }
+                        WildDocValue::Object(map) => {
+                            let new_obj = v8::Object::new(scope);
+                            obj_stack.push((new_obj, map));
+                            current_obj.set(scope, v8_key.into(), new_obj.into());
+                        }
+                        _ => {
+                            if let Ok(v) = serde_v8::to_v8(scope, value) {
+                                current_obj.set(scope, v8_key.into(), v.into());
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    root
+}
+
+fn wd2v8<'s>(wdv: &WildDocValue, scope: &'s mut HandleScope) -> Option<v8::Local<'s, v8::Value>> {
+    match wdv {
+        WildDocValue::Binary(v) => {
+            let ab = v8::ArrayBuffer::with_backing_store(
+                scope,
+                &v8::ArrayBuffer::new_backing_store_from_vec(v.to_owned()).make_shared(),
+            );
+            if let Some(v) = v8::Uint8Array::new(scope, ab, 0, ab.byte_length()) {
+                return Some(v.into());
+            }
+        }
+        WildDocValue::Object(map) => {
+            let obj = wdmap2v8obj(map, scope);
+            return Some(obj.into());
+        }
+        _ => {
+            if let Ok(r) = serde_v8::to_v8(scope, wdv) {
+                return Some(r.into());
+            }
+        }
+    }
+
+    None
 }
 
 #[async_trait(?Send)]
@@ -84,23 +154,19 @@ impl WildDocScript for Deno {
                             &*(v8::Local::<v8::External>::cast(state).value()
                                 as *const WildDocState)
                         };
-
                         let key = args
                             .get(0)
                             .to_string(scope)
                             .unwrap()
                             .to_rust_string_lossy(scope);
                         if key == "global" {
-                            let global = state.global();
-                            if let Ok(r) = serde_v8::to_v8(scope, global.lock().to_owned()) {
-                                retval.set(r.into());
-                            }
+                            let r = wdmap2v8obj(state.global().lock().deref(), scope);
+                            retval.set(r.into());
                         } else {
-                            let stack = state.stack();
-                            for stack in stack.lock().iter().rev() {
+                            for stack in state.stack().lock().iter().rev() {
                                 if let Some(v) = stack.get(key.as_bytes()) {
-                                    if let Ok(r) = serde_v8::to_v8(scope, v) {
-                                        retval.set(r.into());
+                                    if let Some(v) = wd2v8(v, scope) {
+                                        retval.set(v);
                                     }
                                     break;
                                 }
@@ -187,9 +253,11 @@ impl WildDocScript for Deno {
         let mut worker = self.worker.lock();
         let scope = &mut worker.js_runtime.handle_scope();
 
+        let code = std::str::from_utf8(code)?;
+
         if let Some(v) = v8::String::new_from_one_byte(
             scope,
-            ("(".to_owned() + std::str::from_utf8(code)? + ")").as_bytes(),
+            ("(".to_owned() + code + ")").as_bytes(),
             NewStringType::Normal,
         )
         .and_then(|code| v8::Script::compile(scope, code, None))
